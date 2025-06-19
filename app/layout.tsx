57f7994b2d4d998c3850b4a4e2a8e3ad0b4c72f4
Markus Trachsel <markus@codechefs.ch>
2025-06-19 20:20:10 +0200

    Added speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 export const metadata: Metadata = {
   title: 'v0 App',
@@ -15,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}<Analytics /></body>
+      <body>{children}<Analytics /><SpeedInsights /></body>
     </html>
   )
 }
